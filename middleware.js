bccfedb7d34e36a175f19ecb0637eabe48519454
braziliandjspools <leverman.ann@gmail.com>
2025-07-12 13:49:04 -0300

    Adicione página dos termos

--- middleware.js
+++ middleware.js
@@ -1,12 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
+// Por padrão, o Clerk protege todas as rotas.
+// A lista 'publicRoutes' define as exceções que todos podem acessar.
+// Como '/admin' não está nesta lista, ele é automaticamente protegido.
 export default clerkMiddleware({
   publicRoutes: [
-    "/", 
-    "/atualizacoes/(.*)", 
-    "/api/folders(.*)",
-    "/pesquisardrive"
-  ]
+    '/',
+    '/atualizacoes/(.*)',
+    '/api/folders(.*)',
+    '/pesquisardrive',
+    '/terms-of-service',
+    '/privacy-policy'
+  ],
 });
 
 export const config = {
